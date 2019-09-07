30816fac66152c60456072a2effb38f822d50399
Karthik Udyawar <54676225+KarthikUdyawar@users.noreply.github.com>
2019-09-07 17:40:57 +0530

    StartUp

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "y":
+    Dice1 = random.randint(1, 6)
+    Dice2 = random.randint(1, 6)
+    Dice = Dice1 + Dice2
+    print("Rolling the dices...")
+    print("The values are....")
+    print(str(Dice1), str(Dice2))
+    print("Total value: " + str(Dice))
+    roll_again = (input("Roll the dices again?"))
